92cbd7c5852199a8070fcf6968846b8df1f8d09e
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-11-22 16:54:00 +0530

    Completed session handling for the authenticated user

--- src/components/App/Index.js
+++ src/components/App/Index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navigation from "../Navigation/Index";
+import * as ROUTES from "../../constants/routes";
 
-import LandingPage from "../Navigation/Index";
+import Navigation from "../Navigation/Index";
+import LandingPage from "../Landing/Index";
 import SignUpPage from "../SignUp/Index";
 import SignInPage from "../SignIn/Index";
 import PasswordForgetPage from "../PasswordForget/Index";
@@ -10,7 +11,7 @@ import HomePage from "../Home/Index";
 import AccountPage from "../Account/Index";
 import AdminPage from "../Admin/Index";
 
-import * as ROUTES from "../../constants/routes";
+import withAuthentication from "../Session/withAuthenticated";
 
 const App = () => {
   return (
@@ -19,15 +20,19 @@ const App = () => {
         <Navigation />
         <hr />
         <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
+        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route
+          exact
+          path={ROUTES.PASSWORD_FORGET}
+          component={PasswordForgetPage}
+        />
+        <Route exact path={ROUTES.HOME} component={HomePage} />
+        <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route exact path={ROUTES.ADMIN} component={AdminPage} />
       </div>
     </Router>
   );
 };
 
-export default App;
+export default withAuthentication(App);
--- src/components/Navigation/Index.js
+++ src/components/Navigation/Index.js
@@ -3,30 +3,42 @@ import { Link } from "react-router-dom";
 
 import SignOutButton from "../SignOut/Index";
 import * as ROUTES from "../../constants/routes";
+import AuthUserContext from "../Session/context";
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
   <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>LANDING</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>HOME</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>ACCOUNT</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>ADMIN</Link>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
+    <AuthUserContext.Consumer>
+      {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+    </AuthUserContext.Consumer>
   </div>
+);
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>LANDING</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>HOME</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>ACCOUNT</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
