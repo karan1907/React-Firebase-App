624563b45d5235d4489f0c98ecf75b02b2d7852f
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-12-01 12:24:38 +0530

    added message form for the chat room and also added the functionality of rendering and sending messages by establishing user message relation

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -1,14 +1,102 @@
-import React from "react";
+import React, { Component } from "react";
 import { compose } from "recompose";
 
-import { withAuthorization, withEmailVerification } from "../Session/Index";
+import {
+  AuthUserContext,
+  withAuthorization,
+  withEmailVerification
+} from "../Session/Index";
+import { withFirebase } from "../Firebase/Index";
 
 const HomePage = () => (
   <div>
     <h1>Home</h1>
     <p>The Homepage is accessible by every signed in user.</p>
+    <Messages />
   </div>
 );
+
+class MessagesBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      loading: false,
+      messages: []
+    };
+  }
+  onChangeText = event => {
+    this.setState({ text: event.target.value });
+  };
+  onCreateMessage = (event, authUser) => {
+    this.props.firebase.messages().push({
+      text: this.state.text,
+      userId: authUser.uid
+    });
+    this.setState({ text: "" });
+    event.preventDefault();
+  };
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.messages().on("value", snapshot => {
+      const messageObject = snapshot.val();
+      if (messageObject) {
+        // conver message list from snapshot
+        const messageList = Object.keys(messageObject).map(key => ({
+          ...messageObject[key],
+          uid: key
+        }));
+        this.setState({ messages: messageList, loading: false });
+      } else {
+        this.setState({ messages: null, loading: false });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.props.firebase.messages().off();
+  }
+
+  render() {
+    const { text, messages, loading } = this.state;
+
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            {loading && <div>Loading....</div>}
+            {messages ? (
+              <MessageList messages={messages} />
+            ) : (
+              <div>There are no messages....</div>
+            )}
+            <form onSubmit={event => this.onCreateMessage(event, authUser)}>
+              <input type="text" value={text} onChange={this.onChangeText} />
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        )}
+      </AuthUserContext.Consumer>
+    );
+  }
+}
+
+const MessageList = ({ messages }) => (
+  <ul>
+    {messages.map(message => (
+      <MessageItem key={message.uid} message={message} />
+    ))}
+  </ul>
+);
+
+const MessageItem = ({ message }) => (
+  <li>
+    <strong>{message.userId}</strong> {message.text}
+  </li>
+);
+
+const Messages = withFirebase(MessagesBase);
+
 const condition = authUser => !!authUser;
 
 export default compose(
