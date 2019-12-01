827f6c9c9f7cd2907432e8a6ce2bf236562a8510
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-12-01 09:41:58 +0530

    Added firebase react-router for single user page in admin section also fixed exact in app/index.js

--- src/components/Admin/Index.js
+++ src/components/Admin/Index.js
@@ -1,72 +1,166 @@
 import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import { compose } from "recompose";
+
 import { withFirebase } from "../Firebase/Index";
 import { withAuthorization, withEmailVerification } from "../Session/Index";
 import * as ROLES from "../../constants/roles";
-class AdminPage extends Component {
+import * as ROUTES from "../../constants/routes";
+
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>The Admin Page is accessible by every signed in admin user.</p>
+
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
+);
+
+class UserListBase extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       loading: false,
       users: []
     };
   }
+
   componentDidMount() {
     this.setState({ loading: true });
+
     this.props.firebase.users().on("value", snapshot => {
       const usersObject = snapshot.val();
-      const userList = Object.keys(usersObject).map(key => ({
+
+      const usersList = Object.keys(usersObject).map(key => ({
         ...usersObject[key],
         uid: key
       }));
+
       this.setState({
-        users: userList,
+        users: usersList,
         loading: false
       });
     });
   }
+
   componentWillUnmount() {
     this.props.firebase.users().off();
   }
 
   render() {
     const { users, loading } = this.state;
+
     return (
       <div>
-        <h1>Admin</h1>
-        <p>The Admin Page is accessible by every signed in admin user</p>
-        {loading && <div>Loading.....</div>}
-        <UserList users={users} />
+        <h2>Users</h2>
+        {loading && <div>Loading ...</div>}
+        <ul>
+          {users.map(user => (
+            <li key={user.uid}>
+              <span>
+                <strong>ID:</strong> {user.uid}
+              </span>
+              <span>
+                <strong>E-Mail:</strong> {user.email}
+              </span>
+              <span>
+                <strong>Username:</strong> {user.username}
+              </span>
+              <span>
+                <Link
+                  to={{
+                    pathname: `${ROUTES.ADMIN}/${user.uid}`,
+                    state: { user }
+                  }}
+                >
+                  Details
+                </Link>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong>
-          {user.uid}
-        </span>
-        <span>
-          <strong>E-Mail:</strong>
-          {user.email}
-        </span>
-        <span>
-          <strong>Username:</strong>
-          {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
+class UserItemBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      user: null,
+      ...props.location.state
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.user) {
+      return;
+    }
+
+    this.setState({ loading: true });
+
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .on("value", snapshot => {
+        this.setState({
+          user: snapshot.val(),
+          loading: false
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+  }
+
+  onSendPasswordResetEmail = () => {
+    this.props.firebase.doPasswordReset(this.state.user.email);
+  };
+
+  render() {
+    const { user, loading } = this.state;
+
+    return (
+      <div>
+        <h2>User ({this.props.match.params.id})</h2>
+        {loading && <div>Loading ...</div>}
+
+        {user && (
+          <div>
+            <span>
+              <strong>ID:</strong> {user.uid}
+            </span>
+            <span>
+              <strong>E-Mail:</strong> {user.email}
+            </span>
+            <span>
+              <strong>Username:</strong> {user.username}
+            </span>
+            <span>
+              <button type="button" onClick={this.onSendPasswordResetEmail}>
+                Send Password Reset
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+const UserList = withFirebase(UserListBase);
+const UserItem = withFirebase(UserItemBase);
 
 const condition = authUser => authUser && authUser.roles.includes(ROLES.ADMIN);
 
 export default compose(
   withEmailVerification,
-  withAuthorization(condition),
-  withFirebase
+  withAuthorization(condition)
 )(AdminPage);
